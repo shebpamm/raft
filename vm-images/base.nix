1890cc848ebd54ea28860fd04eee58a50d802e31
shebpamm <erik@opcode.xyz>
2023-02-28 22:03:34 +0200

    add root and enable ssh in base image

--- vm-images/base.nix
+++ vm-images/base.nix
@@ -1,2 +1,22 @@
+{ config, pkgs, system, ... }:
 {
+  system.stateVersion = "22.05";
+  boot.initrd.checkJournalingFS = false;
+  services.openssh =
+    {
+      enable = true;
+      settings.permitRootLogin = "yes";
+      extraConfig = ''
+        PubkeyAcceptedKeyTypes +ssh-rsa
+      '';
+    };
+
+  users = {
+    mutableUsers = false;
+    users = {
+      root = {
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK2zXSYBvkCarq9hsQmYAilLbrCaFqDaW7eV8S1nK+oC shebpamm@sorsa.cloud" ];
+      };
+    };
+  };
 }

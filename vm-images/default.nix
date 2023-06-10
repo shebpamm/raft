9a801a528ec3901bd30b6f560be1464ed060e4ad
Erik Karsten <erik@opcode.xyz>
2023-06-10 18:29:23 +0300

    make vm-images support only x86_64-linux, unify the outputs a bit

--- vm-images/default.nix
+++ vm-images/default.nix
@@ -1,41 +1,33 @@
-{ inputs }:
+{ inputs, ... }:
 let
-  bases = inputs.flake-utils.lib.eachDefaultSystem (system:
-    let
-      customGenerators = names:
-        builtins.map
-          (name: {
-            inherit name;
-            module = ./generators/${name}.nix;
-          })
-          names;
-      defaultGenerators = names:
-        builtins.map
-          (name: {
-            inherit name;
-            module = inputs.nixpkgs + "/nixos/modules/virtualisation/${name}.nix";
-          })
-          names;
-      createVM = generator: {
-        "${generator.name}" = inputs.nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            generator.module
-            ./base.nix
-          ];
-        };
-      };
-    in
-    builtins.foldl'
-      (acc: type:
-        createVM type
-        // acc)
-      { }
-      ((defaultGenerators [ "virtualbox-image" "vmware-image" "lxc-container" ])));
+  system = "x86_64-linux";
+  defaultGenerators = names:
+    builtins.map
+      (name: {
+        inherit name;
+        module = inputs.nixpkgs + "/nixos/modules/virtualisation/${name}.nix";
+      })
+      names;
+
+  createVM = generator: {
+    "${generator.name}" = inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        generator.module
+        ./base.nix
+      ];
+    };
+  };
+
+  bases = builtins.foldl'
+    (acc: type:
+      createVM type
+      // acc)
+    { }
+    ((defaultGenerators [ "virtualbox-image" "vmware-image" "lxc-container" ]));
   esxi = import ./esxi.nix { inherit inputs; };
 
 in
 {
-  nixosConfigurations."base" = bases;
-  nixosImages.esxi = esxi;
+  nixosImages = { esxi = esxi; } // bases;
 }

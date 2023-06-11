174138419ead19fc4bb3d02341a54df9a106db88
Erik Karsten <erik@opcode.xyz>
2023-06-11 18:32:46 +0300

    remove fs settings from colmena base

--- colmena/common.nix
+++ colmena/common.nix
@@ -12,17 +12,6 @@
     '';
   };
 
-  fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
-    autoResize = true;
-  };
-  boot = {
-    growPartition = true;
-    loader.grub.device = "/dev/sda";
-    loader.timeout = 0;
-  };
-
   environment.systemPackages = with pkgs;
     [
       htop

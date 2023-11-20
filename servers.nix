e21762914010b70b8e9b10beb228fabc419f79b6
Erik Karsten <erik@opcode.xyz>
2023-11-20 20:03:01 +0200

    provision kubes up to disk stage

--- servers.nix
+++ servers.nix
@@ -1,22 +1,4 @@
 {
-  coalesce = {
-    count = 1;
-    type = "esxi";
-    network = {
-      ipv4pool = "192.168.7.48/28";
-    };
-    traits = [
-      "esxi"
-      "fish"
-      "static"
-      "consul"
-    ];
-    specs = {
-      cpus = 1;
-      memory = 1024;
-      disk = 10; # Gigabytes, this is the data disk. The OS disk is 32GB.
-    };
-  };
   kubernetes = {
     count = 3;
     type = "esxi";
@@ -32,23 +14,6 @@
     specs = {
       cpus = 4;
       memory = 4096;
-      disk = 10; # Gigabytes, this is the data disk. The OS disk is 32GB.
-    };
-  };
-  testing = {
-    count = 1;
-    type = "esxi";
-    network = {
-      ipv4pool = "192.168.7.32/32";
-    };
-    traits = [
-      "esxi"
-      "fish"
-      "static"
-    ];
-    specs = {
-      cpus = 1;
-      memory = 1024;
       disk = 10; # Gigabytes, this is the data disk. The OS disk is 32GB.
     };
   };
